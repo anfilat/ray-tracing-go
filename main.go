e0b29db147866b652898c3387e6ebd8dc00c8509
Andrey Filatkin <anfilat@mail.ru>
2025-02-15 10:54:52 +0300

    adding a Progress Indicator

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	writeImage()
@@ -13,6 +16,7 @@ func writeImage() {
 	fmt.Printf("P3\n%d %d\n255\n", width, height)
 
 	for y := 0; y < height; y++ {
+		fmt.Fprintf(os.Stderr, "\rScanlines remaining: %d ", height-y)
 		for x := 0; x < width; x++ {
 			r := float64(x) / float64(width-1)
 			g := float64(y) / float64(height-1)
@@ -24,4 +28,6 @@ func writeImage() {
 			fmt.Printf("%d %d %d\n", red, green, blue)
 		}
 	}
+
+	fmt.Fprint(os.Stderr, "\rDone                          \n")
 }
