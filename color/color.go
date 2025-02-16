5ff59d61349fc531ca3dd176f3b5918c7749592d
Andrey Filatkin <anfilat@mail.ru>
2025-02-16 20:42:42 +0300

    Using Gamma Correction for Accurate Color Intensity

--- color/color.go
+++ color/color.go
@@ -3,6 +3,7 @@ package color
 import (
 	"fmt"
 	"io"
+	"math"
 
 	"github.com/anfilat/ray-tracing-go.git/interval"
 	"github.com/anfilat/ray-tracing-go.git/vec3"
@@ -23,6 +24,11 @@ func Write(w io.Writer, pixelColor Color) {
 	g := pixelColor.Y()
 	b := pixelColor.Z()
 
+	// Apply a linear to gamma transform for gamma 2
+	r = linearToGamma(r)
+	g = linearToGamma(g)
+	b = linearToGamma(b)
+
 	// Translate the [0,1] component values to the byte range [0,255].
 	intensity := interval.New(0.000, 0.999)
 	rByte := int(256 * intensity.Clamp(r))
@@ -30,4 +36,12 @@ func Write(w io.Writer, pixelColor Color) {
 	bByte := int(256 * intensity.Clamp(b))
 
 	fmt.Fprintf(w, "%d %d %d\n", rByte, gByte, bByte)
+}
+
+func linearToGamma(linearComponent float64) float64 {
+	if linearComponent > 0 {
+		return math.Sqrt(linearComponent)
+	}
+
+	return 0
 }
